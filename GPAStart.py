6c5f8b5ec90191b5e1748bb19dff56e225f1276e
s-hend-185 <146485919+s-hend-185@users.noreply.github.com>
2023-09-29 15:37:35 -0500

    Add files via upload

---
+++ GPAStart.py
@@ -0,0 +1,25 @@
+gpaOne=4
+creditsOne=6
+weightedOne=gpaOne*creditsOne
+
+gpaTwo=3
+creditsTwo=5
+weightedTwo=gpaTwo*creditsTwo
+
+gpaThree=2
+creditsThree=1
+weightedThree=gpaThree*creditsThree
+
+totalCredits = creditsOne+creditsTwo+creditsThree
+totalWeighted = weightedOne+weightedTwo+weightedThree
+
+finalGPA = round (totalWeighted/totalCredits, 2) 
+print ('Your GPA is: ', finalGPA)
+
+# launch IDLE and run this file
+# explain comments
+# fix the error
+# add line numbers
+# better print line (concatenate it)
+# round the result
+# re-organize my code
